7aba629fedaa6cd47c3340d6329f2d1eafe4be9e
Teodor Semerdzhiev <tsemerdzhiev@vmobile.eu>
2024-08-05 10:10:39 +0300

    Empty Spring project added

---
+++ src/main/java/bg/softuni/GimyApi/GimyApiApplication.java
@@ -0,0 +1,13 @@
+package bg.softuni.GimyApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GimyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GimyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bg/softuni/GimyApi/GimyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.GimyApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GimyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
