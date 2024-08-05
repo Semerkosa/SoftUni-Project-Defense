11c01e38327dc7658a5efd6aa6ec69e472a4d38d
Semerkosa <semerkosa@abv.bg>
2024-08-05 16:02:41 +0300

    Added server settings and a home controller

--- client/project/src/environments/environment.ts
+++ client/project/src/environments/environment.ts
@@ -6,7 +6,7 @@ import { HttpHeaders } from "@angular/common/http";
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5000', // json-server
+  apiUrl: 'http://localhost:8080',
   httpOptions: {
     headers: new HttpHeaders({
       "Content-Type": "application/json"
